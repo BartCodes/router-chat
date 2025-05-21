a8658ba257a3b310063193c39605b098e4344d6d
BartCodes <szukala.bartosz@gmail.com>
2025-05-21 15:49:35 +0200

    Fix lint error

--- components/ui/container-text-flip.tsx
+++ components/ui/container-text-flip.tsx
@@ -33,7 +33,7 @@ export function ContainerTextFlip({
   const updateWidthForWord = () => {
     if (textRef.current) {
       // Add some padding to the text width (30px on each side)
-      // @ts-ignore
+      // @ts-expect-error textRef.current may not be an Element with scrollWidth initially
       const textWidth = textRef.current.scrollWidth + 30;
       setWidth(textWidth);
     }
